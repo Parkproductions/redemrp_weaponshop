a9296fc42f185abe4dd12daa06c3009af09e6c73
Parkproductions <declanholt@parkproductions.net>
2020-07-13 21:21:20 +1200

    added client event trigger

--- server.lua
+++ server.lua
@@ -35,8 +35,8 @@ AddEventHandler("redemrp_gunshop:buygun", function(name, price, weapon, lvl)
     if (pw + iw) > 50 then 
         local drop = (pw + iw) - 50
         print('source', source, '_source', _source)
-        TriggerClientEvent('rederm_inventory:fuckyou', _source)
-        TriggerClientEvent('item:pickup', _source)
+        
+        TriggerClientEvent('item:pickup', _source, name, 100, GetHashKey(weapon))
         TriggerClientEvent("redemrp_notification:start", _source, "You Are Carrying Too Much Weight", 3, "success")
         
     end
